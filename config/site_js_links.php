8327e5bec8f3400b54b9768b12f2dbc1164fb4a5
abbas <mabbaskhan571@gmail.com>
2026-01-17 09:39:11 -0800

    create config and assets folder
    create config and assets folder

---
+++ config/site_js_links.php
@@ -0,0 +1,7 @@
+ <script src="./assets/js/plugins/simplebar.min.js"></script>
+    <script src="./assets/js/plugins/popper.min.js"></script>
+    <script src="./assets/js/icon/custom-icon.js"></script>
+    <script src="./assets/js/plugins/feather.min.js"></script>
+    <script src="./assets/js/component.js"></script>
+    <script src="./assets/js/theme.js"></script>
+    <script src="./assets/js/script.js"></script>
